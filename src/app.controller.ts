c56cce4d2d1f6eb3c97bb433fbe5c88889d1dc9e
内藤祐馬 <naitoyuma@outlook.jp>
2023-03-01 17:18:06 +0900

    db詳細migrate,seed作成, REST user完成

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+// import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
@@ -10,15 +10,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-}
 
-@Controller('user')
-export class TodoController {
-  constructor(private prisma: PrismaService) {}
-
-  @Get('list')
-  async getList() {
-    const result = await this.prisma.user.findMany();
-    return [...result];
+  @Get('hi')
+  getHi(): string {
+    return 'hi';
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './service/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserController } from './controller/user.controller';
 
-@Module({})
+@Module({
+  providers: [UserService, PrismaService],
+  controllers: [UserController],
+})
 export class UserModule {}
