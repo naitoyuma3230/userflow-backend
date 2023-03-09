6f7e300731202ae2d353ad66fb4b9603ea84d343
内藤祐馬 <naitoyuma@outlook.jp>
2023-03-10 08:16:34 +0900

    gitキャッシュ削除 gitignore更新

--- src/openai/openai.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OpenaiService } from './service/openai.service';
-import { OpenaiController } from './controller/openai.controller';
-
-@Module({
-  controllers: [OpenaiController],
-  providers: [OpenaiService],
-})
-export class OpenaiModule {}
